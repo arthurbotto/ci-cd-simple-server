70238cd3ac51880db3dfbcfb1beaf8b36fb8cc99
arthurbotto <arthurgsbotto@hotmail.com>
2025-01-16 14:55:14 +0000

    hero msg

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def get_home():
     return "Hello, world!"
 
+@app.route('/hero', methods=['GET'])
+def get_hero_msg():
+    return "I am a CI-CD Hero , I guess.."
+
 if __name__ == '__main__':
     app.run(
       debug=True,
